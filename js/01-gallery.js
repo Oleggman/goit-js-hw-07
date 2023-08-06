a36e7f2819a3700fe0faff6b7041d3166034e507
Олег <olegkovalets1112003@knu.ua>
2023-08-06 14:53:36 +0300

    Close lightbox by Escape in task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryMarkup = galleryItems
+  .map(
+    (img) =>
+      `<li class="gallery__item">
+            <a class="gallery__link" href="${img.original}">
+                <img
+                    class="gallery__image"
+                    src="${img.preview}"
+                    data-source="${img.original}"
+                    alt="${img.description}"
+                />
+            </a>
+        </li>`
+  )
+  .join("");
+
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryRef.addEventListener("click", onGalleryItemClick);
+
+let instance;
+
+function onGalleryItemClick(evt) {
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+
+  evt.preventDefault();
+
+  instance = basicLightbox.create(`<img src="${evt.target.dataset.source}" alt="${evt.target.alt}" />`);
+  instance.show();
+
+  closeModalByEscape();
+}
+
+function closeModalByEscape() {
+  window.addEventListener("keydown", onEscPress);
+
+  function onEscPress(evt) {
+    if (!instance.visible()) {
+      window.removeEventListener("keydown", onEscPress);
+      return;
+    }
+
+    if (evt.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onEscPress);
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryMarkup = galleryItems
+  .map(
+    (img) =>
+      `<li class="gallery__item">
+            <a class="gallery__link" href="${img.original}">
+                <img class="gallery__image" src="${img.preview}" alt="${img.description}" title="${img.description}"/>
+            </a>
+        </li>`
+  )
+  .join("");
+
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
